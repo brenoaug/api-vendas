302d75529a5ddbf889ede1413a7d10ec3eaafea7
breno augusto <breaugustocp@outlook.com>
2025-12-29 17:07:24 -0300

    criando mensagens de logs

--- src/main/java/com/recode/hanami/controller/CsvController.java
+++ src/main/java/com/recode/hanami/controller/CsvController.java
@@ -1,12 +1,14 @@
 package com.recode.hanami.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.recode.hanami.dto.DadosArquivoDTO;
 import com.recode.hanami.dto.ImportacaoResponseDTO;
 import com.recode.hanami.exceptions.ArquivoInvalidoException;
 import com.recode.hanami.exceptions.DadosInvalidosException;
 import com.recode.hanami.service.CsvService;
 import com.recode.hanami.service.ProcessamentoVendasService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/csv")
 public class CsvController {
+    private static final Logger logger = LoggerFactory.getLogger(CsvController.class);
 
     private final CsvService csvService;
     private final ProcessamentoVendasService processamentoVendasService;
@@ -31,6 +34,9 @@ public class CsvController {
 
     @PostMapping("/upload")
     public ResponseEntity<?> uploadCsv(@RequestParam(value = "file", required = false) MultipartFile file) {
+        logger.info("Iniciando processamento de upload de arquivo");
+
+
         if (file == null || file.isEmpty()) {
             return ResponseEntity
                     .badRequest()
@@ -41,15 +47,26 @@ public class CsvController {
         }
 
         try {
+            logger.info("Iniciando conversão do arquivo CSV para JSON. Nome do arquivo: {}", file.getOriginalFilename());
             List<DadosArquivoDTO> listaProcessada = csvService.conversorCsvParaJson(file);
+            logger.debug("Arquivo convertido com sucesso. Número de registros: {}", listaProcessada.size());
+
+            logger.info("Iniciando salvamento dos dados do arquivo");
             processamentoVendasService.salvarDadosDoArquivo(listaProcessada);
+            logger.info("Dados salvos com sucesso");
+
+
 
             ImportacaoResponseDTO resposta = new ImportacaoResponseDTO("sucesso", listaProcessada.size());
+            logger.info("Processo de upload finalizado com sucesso. Registros processados: {}", listaProcessada.size());
+
             return ResponseEntity
                     .ok()
                     .body(resposta);
 
         } catch (ArquivoInvalidoException | DadosInvalidosException e) {
+            logger.error("Erro de dados inválidos: {}", e.getMessage());
+
             return ResponseEntity
                     .unprocessableContent()
                     .body(Map.of(
@@ -57,7 +74,7 @@ public class CsvController {
                             "mensagem", e.getMessage()
                     ));
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Erro inesperado durante o processamento do arquivo", e);
             return ResponseEntity
                     .internalServerError()
                     .body(Map.of(
